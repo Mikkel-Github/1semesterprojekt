add02332889462608da6499f4569e043e8c4d196
Mikkel <mikkelpriisholm@gmail.com>
2020-10-27 14:28:56 +0100

    Fixed World of Sult

--- WorldOfSult/zuul-framework/worldofzuul/Game.java
+++ WorldOfSult/zuul-framework/worldofzuul/Game.java
@@ -15,18 +15,17 @@ public class Game
 
     private void createRooms()
     {
-        Room outside, theatre, pub, lab, office, uganda;
+        Room outside, theatre, pub, lab, office;
       
         outside = new Room("outside the main entrance of the university");
         theatre = new Room("in a lecture theatre");
         pub = new Room("in the campus pub");
         lab = new Room("in a computing lab");
         office = new Room("in the computing admin office");
-        uganda = new Room("in Uganda");
         
         outside.setExit("east", theatre);
         outside.setExit("south", lab);
-        outside.setExit("west", uganda);
+        outside.setExit("west", pub);
 
         theatre.setExit("west", outside);
 
@@ -36,9 +35,6 @@ public class Game
         lab.setExit("east", office);
 
         office.setExit("west", lab);
-
-        uganda.setExit("east", theatre);
-        uganda.setExit("west", office);
 
         currentRoom = outside;
     }

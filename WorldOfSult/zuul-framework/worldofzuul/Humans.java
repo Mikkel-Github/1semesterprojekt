ce15ce3dd35f402fe229e2e8ece91a596293941e
Lucas <Luhavenkvist@yahoo.dk>
2020-11-05 14:15:32 +0100

    rettede mine egne fejl

--- WorldOfSult/zuul-framework/worldofzuul/Humans.java
+++ WorldOfSult/zuul-framework/worldofzuul/Humans.java
@@ -7,14 +7,8 @@ public class Humans {
     public String name;
     public int age;
 
-    public Humans(String p_name, int p_age)
-    {
+    public Humans(String p_name, int p_age) {
         name = p_name;
         age = p_age;
-
-
     }
-
-
-
 }

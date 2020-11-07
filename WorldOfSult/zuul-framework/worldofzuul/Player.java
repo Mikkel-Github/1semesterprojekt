d02a5eb9bc89072d05a82b9063b9ee24bfda1151
Mikkel <mikkelpriisholm@gmail.com>
2020-11-07 17:47:59 +0100

    Opdatering af mergeable fil

--- WorldOfSult/zuul-framework/worldofzuul/Player.java
+++ WorldOfSult/zuul-framework/worldofzuul/Player.java
@@ -66,7 +66,7 @@ public class Player {
         return playerBalance;
     }
 
-    
+
     ///////////////////////////// PLAYER QUIZ //////////////////////////////////
     // Adds a point to the players score
     public void addPoint() {

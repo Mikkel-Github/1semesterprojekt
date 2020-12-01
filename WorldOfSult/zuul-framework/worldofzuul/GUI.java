3e4ba629bf0b5d89407ca373e03221ad6d809b0c
Mikkel <mikkelpriisholm@gmail.com>
2020-12-01 14:52:39 +0100

    Opdaatering af size

--- WorldOfSult/zuul-framework/worldofzuul/GUI.java
+++ WorldOfSult/zuul-framework/worldofzuul/GUI.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.awt.*;
+
 
 public class GUI extends Application {
     // Der skal ikke laves så meget i GUI
@@ -15,10 +17,17 @@ public class GUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int width = gd.getDisplayMode().getWidth();
+        int height = gd.getDisplayMode().getHeight();
+
         GUI.primaryStage = primaryStage;
         GUI.primaryStage.initStyle(StageStyle.UNDECORATED);
         GUI.primaryStage.setX(0);
         GUI.primaryStage.setY(0);
+
+        GUI.primaryStage.setWidth(width);
+        GUI.primaryStage.setHeight(height);
 
         StageController stageController = new StageController();
         stageController.changeScene("Menu");
